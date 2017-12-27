83a8e66dbd25a6bf934c72d00dc5405283c18b59
Arnav Gupta <arnav@codingblocks.com>
2017-12-27 20:26:14 +0530

    using port from env if available
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 
+const SERVER_PORT = process.env.PORT || 3333
+
 const app = express()
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.send("Hello from the backend"))
 
-app.listen(3333, () => console.log("Server started"))
+app.listen(SERVER_PORT, () => console.log("Server started"))
